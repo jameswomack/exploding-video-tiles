3de4717dc2d081574532784059f9bd92f74f2ae9
James Womack <jwomack@netflix.com>
2016-02-09 18:05:40 -0800

    Real-time scanning

--- js/analyzeColors.js
+++ js/analyzeColors.js
@@ -1,249 +1,218 @@
-  function hslToColor(hslArray) {
-    //converts hue to color groups
-    hslArray = hslArray.map(function(d){
-        var hue = d.h;
+function hueBar(svgSel, hslArray) {
+  const svg = d3.select(svgSel),
+    width = 600,
+    height = 200;
 
-        if (hue < 23){
-            d.group = "red";
-        } else if (hue < 42){
-            d.group = "orange";
-        } else if (hue < 70){
-            d.group = "yellow";
-        } else if (hue < 164){
-            d.group = "green";
-        } else if (hue < 203){
-            d.group = "cyan";
-        } else if (hue < 262){
-            d.group = "blue";
-        } else if (hue < 299){
-            d.group = "purple";
-        } else if (hue < 337){
-            d.group = "magenta";
-        } else {
-            d.group = "red";
-        }
+  svg.attr('width', width)
+    .attr('height', height);
 
-        return d;
-    });
+    var colorHistogram = d3.nest()
+        .key(function(d){ return d.group; })
+        .key(function(d){ return Math.round(d.h); })
+        .rollup(function(leaves){ return leaves.length; })
+        .entries(hslArray);
 
-    return hslArray;
-  }
+  var x = d3.scale.linear().domain([0, 360]).range([30, width - 30]),
+  y = d3.scale.linear().range([30, height -30]),
+  max = 0;
 
-  function hueBar(svgSel, hslArray) {
-    const svg = d3.select(svgSel),
-      width = 600,
-      height = 200;
-
-    svg.attr('width', width)
-      .attr('height', height);
-
-      var colorHistogram = d3.nest()
-          .key(function(d){ return d.group; })
-          .key(function(d){ return Math.round(d.h); })
-          .rollup(function(leaves){ return leaves.length; })
-          .entries(hslArray);
-
-    var x = d3.scale.linear().domain([0, 360]).range([30, width - 30]),
-    y = d3.scale.linear().range([30, height -30]),
-    max = 0;
-
-    colorHistogram.forEach(function(group){
-        var groupMax = d3.max(group.values, function(d){
-          return d.values;
-        })
-
-        if (groupMax > max){
-          max = groupMax;
-        }
-    })
-
-    y.domain([0, max]);
-
-    svg.selectAll('rect')
-      .remove()
-
-    colorHistogram.forEach(function(group){
-        svg.selectAll('rect.test')
-          .data(group.values)
-          .enter()
-          .append('rect')
-          .attr('x', function(d) { return x(d.key)})
-          .attr('y', function(d) { return height - y(d.values)})
-          .attr('height', function(d) {
-              return y(d.values)})
-          .attr('width', 2)
-          .attr('fill', function(d){ return 'hsl(' + d.key +', 80%, 50%)'})
-
-    })
-  }
-
-  function hueLightScatter(svgSel, hslArray) {
-    const svg = d3.select(svgSel),
-      width = 800,
-      height = 600;
-
-    svg.attr('width', width)
-      .attr('height', height);
-
-    var x = d3.scale.linear().domain([0, 360]).range([30, width - 30]),
-    y = d3.scale.linear().domain([0, 100]).range([30, height -30]);
-
-    svg.selectAll('circle')
-        .data(hslArray)
-        .enter()
-        .append('circle')
-        .attr('r', 1)
-        .attr('cx', function(d) { return x(d.h)})
-        .attr('cy', function(d) { return y(d.l)})
-        .attr('fill', function(d){ return 'hsl(' + d.h +',' + d.s + '%,' + d.l +'%)'})
-
-
-  }
-
-  function colorMap(svgSel, hslArray) {
-    const svg = d3.select(svgSel),
-      width = 800,
-      height = 400;
-
-    svg.attr('width', width)
-      .attr('height', height);
-
-    // group lightness into buckets
-    console.log('about to re create lightness histogram')
-    var lightnessHistogram = d3.nest()
-      .key(function(d){ return Math.round(d.l); })
-      .rollup(function(leaves){
-
-            var swatches = {};
-            leaves.forEach(function(l){
-                var h = Math.round(l.h),
-                s = Math.round(l.s),
-                key = h + ',' + s + '%';
-
-                if (!swatches[key]){
-                    swatches[key] = 1;
-                } else {
-                    swatches[key] = swatches[key] + 1;
-                }
-            });
-
-        return {
-            total: leaves.length,
-            values: swatches
-        };
-
+  colorHistogram.forEach(function(group){
+      var groupMax = d3.max(group.values, function(d){
+        return d.values;
       })
-      .entries(hslArray);
 
-    var margin = {bottom: 30, left: 30, right: 100, top: 30},
-    sWidth = width - margin.left - margin.right,
-    sHeight = height- margin.bottom - margin.top,
-    x = d3.scale.linear().domain([0, 360]).range([margin.left, sWidth]), //hue
-    y = d3.scale.linear().domain([100, 0]).range([margin.top, sHeight]), //saturation & lightness
-    barW = d3.scale.linear().range([0, margin.right]),
-    xAxis = d3.svg.axis().scale(x),
-    yAxis = d3.svg.axis().scale(y).orient('left'),
-    r = d3.scale.sqrt().range([1, 10]),
-    maxLightness = { key: '', total: 0};
+      if (groupMax > max){
+        max = groupMax;
+      }
+  })
 
+  y.domain([0, max]);
 
-    lightnessHistogram.forEach(function(d){
-        if (d.values.total > maxLightness.total){
-            maxLightness.total = d.values.total;
-            maxLightness.key = d.key;
-        }
-    });
+  svg.selectAll('rect')
+    .remove()
 
-    barW.domain([0, maxLightness.total]);
+  colorHistogram.forEach(function(group){
+      svg.selectAll('rect.test')
+        .data(group.values)
+        .enter()
+        .append('rect')
+        .attr('x', function(d) { return x(d.key)})
+        .attr('y', function(d) { return height - y(d.values)})
+        .attr('height', function(d) {
+            return y(d.values)})
+        .attr('width', 2)
+        .attr('fill', function(d){ return 'hsl(' + d.key +', 80%, 50%)'})
 
-    redraw(maxLightness.key);
+  })
+}
 
-    function clickedBar(d){
-        redraw(d.key);
-    }
+function hueLightScatter(svgSel, hslArray) {
+  const svg = d3.select(svgSel),
+    width = 800,
+    height = 600;
 
-    function redraw(currentKey){
-        var maxValues = lightnessHistogram.filter(function(d){
-            return d.key === currentKey;
-        })[0].values.values;
+  svg.attr('width', width)
+    .attr('height', height);
 
-        var maxCount = 0;
+  var x = d3.scale.linear().domain([0, 360]).range([30, width - 30]),
+  y = d3.scale.linear().domain([0, 100]).range([30, height -30]);
 
-        for (var k in maxValues){
-            if (maxValues[k] > maxCount){
-                maxCount = maxValues[k];
-            }
-        }
-
-        r.domain([1, maxCount]);
-
-
-        svg.selectAll('g.axis')
-            .remove();
-
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + (height - margin.top) + ")")
-            .call(xAxis);
-
-        svg.append("g")
-            .attr("class", "y axis")
-            .attr("transform", "translate(" + margin.left + ",0)")
-            .call(yAxis);
-
-        svg.append("g")
-            .attr("class", "y-bar axis")
-            .attr("transform", "translate(" + (margin.left + sWidth) + ",0)")
-            .call(yAxis);
-
-        var circles = svg.selectAll('circle')
-           .data(Object.keys(maxValues));
-
-        circles.exit()
-           .transition()
-           .duration(500)
-           .attr('r', 0)
-           .remove();
-
-        circles.enter()
-           .append('circle');
-
-        circles
-           .attr('cx', function(d) { return x(parseInt(d.split(',')[0]))})
-           .attr('cy', function(d) { return y(parseInt(d.split(',')[1]))})
-           .attr('fill', function(d){ return 'hsl(' + d + ',' + currentKey +'%)'})
-           .transition()
-           .duration(500)
-           .attr('r', function(d){ return r(maxValues[d])})
-
-        var bars = svg.selectAll('rect')
-            .data(lightnessHistogram);
-
-        bars.enter()
-            .append('rect')
-            .attr('height', 3);
-
-        bars.attr('x', margin.left + sWidth)
-            .attr('y', function(d){ return y(parseInt(d.key)); })
-            .attr('class', function(d){
-                if (d.key === currentKey){
-                    return 'highlight';
-                } else {
-                    return '';
-                }
-            })
-            .on('click', clickedBar)
-            .transition()
-            .attr('width', function(d){ return barW(d.values.total); })
+  svg.selectAll('circle')
+      .data(hslArray)
+      .enter()
+      .append('circle')
+      .attr('r', 1)
+      .attr('cx', function(d) { return x(d.h)})
+      .attr('cy', function(d) { return y(d.l)})
+      .attr('fill', function(d){ return 'hsl(' + d.h +',' + d.s + '%,' + d.l +'%)'})
 
 
-        bars.exit()
-            .transition()
-            .attr('width', 0)
-            .remove();
+}
 
-    }
+function colorMap(svgSel, hslArray) {
+  const svg = d3.select(svgSel),
+    width = 800,
+    height = 400;
+
+  svg.attr('width', width)
+    .attr('height', height);
+
+  // group lightness into buckets
+  console.log('about to re create lightness histogram')
+  var lightnessHistogram = d3.nest()
+    .key(function(d){ return Math.round(d.l); })
+    .rollup(function(leaves){
+
+          var swatches = {};
+          leaves.forEach(function(l){
+              var h = Math.round(l.h),
+              s = Math.round(l.s),
+              key = h + ',' + s + '%';
+
+              if (!swatches[key]){
+                  swatches[key] = 1;
+              } else {
+                  swatches[key] = swatches[key] + 1;
+              }
+          });
+
+      return {
+          total: leaves.length,
+          values: swatches
+      };
+
+    })
+    .entries(hslArray);
+
+  var margin = {bottom: 30, left: 30, right: 100, top: 30},
+  sWidth = width - margin.left - margin.right,
+  sHeight = height- margin.bottom - margin.top,
+  x = d3.scale.linear().domain([0, 360]).range([margin.left, sWidth]), //hue
+  y = d3.scale.linear().domain([100, 0]).range([margin.top, sHeight]), //saturation & lightness
+  barW = d3.scale.linear().range([0, margin.right]),
+  xAxis = d3.svg.axis().scale(x),
+  yAxis = d3.svg.axis().scale(y).orient('left'),
+  r = d3.scale.sqrt().range([1, 10]),
+  maxLightness = { key: '', total: 0};
 
 
+  lightnessHistogram.forEach(function(d){
+      if (d.values.total > maxLightness.total){
+          maxLightness.total = d.values.total;
+          maxLightness.key = d.key;
+      }
+  });
+
+  barW.domain([0, maxLightness.total]);
+
+  redraw(maxLightness.key);
+
+  function clickedBar(d){
+      redraw(d.key);
+  }
+
+  function redraw(currentKey){
+      var maxValues = lightnessHistogram.filter(function(d){
+          return d.key === currentKey;
+      })[0].values.values;
+
+      var maxCount = 0;
+
+      for (var k in maxValues){
+          if (maxValues[k] > maxCount){
+              maxCount = maxValues[k];
+          }
+      }
+
+      r.domain([1, maxCount]);
+
+
+      svg.selectAll('g.axis')
+          .remove();
+
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + (height - margin.top) + ")")
+          .call(xAxis);
+
+      svg.append("g")
+          .attr("class", "y axis")
+          .attr("transform", "translate(" + margin.left + ",0)")
+          .call(yAxis);
+
+      svg.append("g")
+          .attr("class", "y-bar axis")
+          .attr("transform", "translate(" + (margin.left + sWidth) + ",0)")
+          .call(yAxis);
+
+      var circles = svg.selectAll('circle')
+         .data(Object.keys(maxValues));
+
+      circles.exit()
+         .transition()
+         .duration(100)
+         .attr('r', 0)
+         .remove();
+
+      circles.enter()
+         .append('circle');
+
+      circles
+         .attr('cx', function(d) { return x(parseInt(d.split(',')[0]))})
+         .attr('cy', function(d) { return y(parseInt(d.split(',')[1]))})
+         .attr('fill', function(d){ return 'hsl(' + d + ',' + currentKey +'%)'})
+         .transition()
+         .duration(10)
+         .attr('r', function(d){ return r(maxValues[d])})
+
+      var bars = svg.selectAll('rect')
+          .data(lightnessHistogram);
+
+      bars.enter()
+          .append('rect')
+          .attr('height', 3);
+
+      bars.attr('x', margin.left + sWidth)
+          .attr('y', function(d){ return y(parseInt(d.key)); })
+          .attr('class', function(d){
+              if (d.key === currentKey){
+                  return 'highlight';
+              } else {
+                  return '';
+              }
+          })
+          .on('click', clickedBar)
+          .transition()
+          .attr('width', function(d){ return barW(d.values.total); })
+
+
+      bars.exit()
+          .transition()
+          .attr('width', 0)
+          .remove();
 
   }
+
+
+
+}
--- js/index.js
+++ js/index.js
@@ -445,13 +445,23 @@
       inputEl.keydown  = onchange
     }
 
-    videoEl.addEventListener('pause', function () {
-
-      const hslValues = hslToColor(hslValuesFromContext(copyCtx));
-      colorMap('#color-analysis svg#colorMap', hslValues);
-      hueBar('#color-analysis svg#hueBar', hslValues);
-      // hueLightScatter('#color-analysis svg#hueScatter', hslValues);
+    var worker = new Worker('js/worker.js');
+    worker.addEventListener('message', function (e) {
+      const hslValues = e.data.result;
+      window.colorMap('#color-analysis svg#colorMap', hslValues);
+      window.hueBar('#color-analysis svg#hueBar', hslValues);
+      console.log('Message received from worker');
     })
+
+    setInterval(function () {
+      var imageCanvas = copyCtx.canvas;
+
+      var b = copyCtx.getImageData(200, 200, imageCanvas.width/4, imageCanvas.height/4);
+
+      worker.postMessage({
+        image : b
+      })
+    }, 1000)
 
     document.addEventListener('keypress', function (keyboardEvent) {
       if ((keyboardEvent.keyCode === 112 || keyboardEvent.keyCode === 32) &&
@@ -467,66 +477,5 @@
 
     window.addEventListener('hashchange', updateTileMode)
   })
-
-  function bound01 (n, max) {
-    n = Math.min(max, Math.max(0, parseFloat(n)))
-
-    // Handle floating point rounding errors
-    if (Math.abs(n - max) < 0.000001)
-      return 1
-    else
-      // Convert into [0, 1] range if it isn't already
-      return n % max / parseFloat(max)
-  }
-
-  function rgbToHsl(r, g, b) {
-    r = bound01(r, 255);
-    g = bound01(g, 255);
-    b = bound01(b, 255);
-
-    var max = Math.max(r, g, b), min = Math.min(r, g, b);
-    var h, s, l = (max + min) / 2;
-
-    if (max == min)
-      h = s = 0; // achromatic
-    else {
-      var d = max - min;
-      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-
-      switch (max) {
-        case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-
-        case g: h = (b - r) / d + 2; break;
-
-        case b: h = (r - g) / d + 4; break;
-      }
-
-      h /= 6;
-    }
-
-    return { h: h * 360, s: s * 100, l: l * 100 };
-  }
-
-  function hslValuesFromContext (ctx) {
-    var imageCanvas = ctx.canvas;
-    var imageData   = ctx.getImageData(0, 0, imageCanvas.width, imageCanvas.height);
-
-    var hslValues = [ ]
-
-    for (var x = 0; x < imageData.width; ++x)
-      for (var y = 0; y < imageData.height; ++y) {
-        var index = (x + y * imageData.width) * 4
-
-        var r = imageData.data[index + 0]
-        var g = imageData.data[index + 1]
-        var b = imageData.data[index + 2]
-        var hsl = rgbToHsl(r, g, b)
-
-        hslValues.push(hsl)
-      }
-
-    return hslValues
-  }
-
 
 })()
